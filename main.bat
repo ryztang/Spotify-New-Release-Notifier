28148955afb069ae60d24bebfc203b9b8170d6b7
Ryan Tang <tangryanz@yahoo.ca>
2020-04-15 17:45:22 -0400

    Add executable batch file and setup batch file

---
+++ main.bat
@@ -0,0 +1,3 @@
+call <Enter-path-to-miniconda-or-anaconda>\Scripts\activate.bat <Enter-path-to-miniconda-or-anaconda>
+call conda activate Spotify-New-Release-Notifier
+call python main.py
---
+++ setup.bat
@@ -0,0 +1,5 @@
+call <Enter-path-to-miniconda-or-anaconda>\Scripts\activate.bat <Enter-path-to-miniconda-or-anaconda>
+call conda create --name Spotify-New-Release-Notifier
+call conda activate Spotify-New-Release-Notifier
+call pip install spotipy
+call conda install pandas
