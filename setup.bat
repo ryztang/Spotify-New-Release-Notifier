bf4d4d573c5b967c002c4c48c8a2cc25ef40f2ad
Ryan Tang <tangryanz@yahoo.ca>
2020-04-15 18:03:37 -0400

    Fixed setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 call <Enter-path-to-miniconda-or-anaconda>\Scripts\activate.bat <Enter-path-to-miniconda-or-anaconda>
 call conda create --name Spotify-New-Release-Notifier
 call conda activate Spotify-New-Release-Notifier
+call conda install pip
 call pip install spotipy
 call conda install pandas
